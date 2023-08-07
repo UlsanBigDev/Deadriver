d8c04e679c1cabca3943df14490868a6ace47cb8
ptk57581 <ptk725739@gmail.com>
2023-08-08 00:31:40 +0900

    Story Manager Add

---
+++ Assets/Script/Story/StoryManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // 테스트
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
