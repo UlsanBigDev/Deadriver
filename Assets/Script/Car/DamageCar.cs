d23ce452307c3e7001109081728e646242279405
snowplum212 <snowplum212@gmail.com>
2023-08-07 23:04:36 +0900

    CarDamage함수 호출 1초 딜레이 추가

--- Assets/Script/Car/DamageCar.cs
+++ Assets/Script/Car/DamageCar.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class DamageCar : AbstractCar
 {
     public int carDamage = 10;
-
+    private bool delayedDamage = false;
     private void OnCollisionEnter(Collision collision) // 충돌이 일어나자 마자
     {
         if (collision.gameObject.CompareTag("building")) // building 이라는 tag 를 가진 오브젝트와 충돌했을때, CompareTag = 자바 Equals랑 비슷한 느낌?
         {
-            CarDamage(carDamage);
+            if (!delayedDamage)
+            {
+                Invoke("DelayedCarDamage", 1f);
+                delayedDamage = true;
+            }
         }
+    }
+    private void DelayedCarDamage()
+    {
+        CarDamage(carDamage);
+        delayedDamage = false;
     }
 }
