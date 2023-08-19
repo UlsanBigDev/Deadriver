0c6779317affe7cb6cb3b6b2b3071d4ec25de631
ASY-030106 <asy951013@naver.com>
2023-08-19 14:04:01 +0900

    시야방해 디버프 끝

--- Assets/Script/Car/Event/SightEvent.cs
+++ Assets/Script/Car/Event/SightEvent.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class SightEvent : DrunkEvent
 {
     public Image image;
+    DrunkLevel level = Player.GetPlayer().drunkLevel;
 
     public SightEvent(Image panel)
     {
@@ -14,9 +15,29 @@ public class SightEvent : DrunkEvent
 
     public void sight()
     {
-        //Debug.Log(image);
         Debug.Log("시야방해 디버프");
-        image.color = new Color(255,255, 255, 0.5F);
+
+        if (level == DrunkLevel.GREEN)
+        {
+           image.color = new Color(255, 255, 255);
+            Debug.Log("GREEN 시야방해");
+        }
+        else if (level == DrunkLevel.YELLOW)
+        {
+            image.color = new Color(255, 255, 255, 0.1F);
+            Debug.Log("YELLOW 시야방해");
+        }
+        else if (level == DrunkLevel.ORANGE)
+        {
+            image.color = new Color(255, 255, 255, 0.2F);
+            Debug.Log("ORANGE 시야방해");
+        }
+        else if (level == DrunkLevel.RED)
+        {
+            image.color = new Color(255, 255, 255, 0.3F);
+            Debug.Log("RED 시야방해");
+        }
+        
     }
 
     public override void Run()
