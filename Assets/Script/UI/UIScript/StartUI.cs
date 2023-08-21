1d24c0f125b146121f9cb8eaf822cdae338c1e83
snowplum212 <snowplum212@gmail.com>
2023-08-22 04:20:33 +0900

    added click event sound and bgm on/off available

--- Assets/Script/UI/UIScript/StartUI.cs
+++ Assets/Script/UI/UIScript/StartUI.cs
@@ -9,31 +9,47 @@ public class StartUI : MonoBehaviour
     public Button start;
     public Button configuration;
     public Image BGM;
+    public UISoundManager uISoundManager;
+    public AudioSource bgmPlayer;
 
+    void Awake()
+    {
+        uISoundManager = FindObjectOfType<UISoundManager>();
+    }
     public void clickconfiguration()
     {
         Debug.Log("환경설정 클릭");
         BGM.gameObject.SetActive(true);
+        uISoundManager.SfxPlay(UISoundManager.Sfx.click);
     }
 
     public void clickClose()
     {
         Debug.Log("환경설정 창 닫기");
         BGM.gameObject.SetActive(false);
+        uISoundManager.SfxPlay(UISoundManager.Sfx.click);
     }
 
     public void clickStart()
     {
         Debug.Log("Story Scene으로 전환");
         SceneManager.LoadScene("StoryLine_Developer");
+        uISoundManager.SfxPlay(UISoundManager.Sfx.click);
     }
 
     public void clickOn()
     {
         Debug.Log("BGM ON");
+        uISoundManager.SfxPlay(UISoundManager.Sfx.click);
+        if(!uISoundManager.bgmPlayer.isPlaying)
+        {
+            uISoundManager.bgmPlayer.Play();
+        }
     }
     public void clickOFF()
     {
         Debug.Log("BGM OFF");
+        uISoundManager.SfxPlay(UISoundManager.Sfx.click);
+        uISoundManager.bgmPlayer.Stop();
     }
 }
