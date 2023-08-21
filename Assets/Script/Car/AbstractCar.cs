bb963a22077be6892a21aec95ef186d374de1e90
ptk57581 <ptk725739@gmail.com>
2023-08-21 13:30:50 +0900

    Abstract Car 리팩토링

--- Assets/Script/Car/AbstractCar.cs
+++ Assets/Script/Car/AbstractCar.cs
@@ -9,6 +9,8 @@ public abstract class AbstractCar : MonoBehaviour, Car
     public float speed { get; set; }
     public float rotation { get; set; }
     public int carHp { get; set; }
+    public int carDamage;
+    private bool delayedDamage = false;
 
     public SoundManager soundManager;
     private CarMovement carMovement;
@@ -67,4 +69,68 @@ public abstract class AbstractCar : MonoBehaviour, Car
         Destroy(this);
     }
 
+    /// <summary>
+    /// Enemy 와 충돌 햇을때 호
+    /// </summary>
+    protected void OnEnemyCrash(Enemy enemy)
+    {
+        if (delayedDamage) return;
+        if (enemy is Building)
+        {
+            carDamage = 5;
+        }
+        else if (enemy is EnemyCar)
+        {
+            carDamage = 3;
+        }
+        else if (enemy is Person)
+        {
+            carDamage = 1;
+        }
+        StartCoroutine(DamageDelay(carDamage, 1f));
+    }
+    private void OnCollisionEnter(Collision collision) // 충돌이 일어나자 마자
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            OnEnemyCrash(collision.gameObject.GetComponent<Enemy>());
+        }
+        //if (collision.gameObject.CompareTag("building")) // building 이라는 tag 를 가진 오브젝트와 충돌했을때, CompareTag = 자바 Equals랑 비슷한 느낌?
+        //{
+        //    if (!delayedDamage)
+        //    {
+        //        //soundManager.SfxPlay(SoundManager.Sfx.crashBuiling);
+        //        carDamage = 10;
+        //        Debug.Log(carDamage);
+        //        StartCoroutine(DamageDelay(carDamage, 1f)); //코루틴으로 순간적으로 충돌이 중복적으로 일어나는 현상을 방지하기 위해 최소한의 딜레이값 설정필요함
+        //    }
+        //}
+        //else if (collision.gameObject.CompareTag("EnemyCar"))
+        //{
+        //    if (!delayedDamage)
+        //    {
+        //        //soundManager.SfxPlay(SoundManager.Sfx.crashBuiling);
+        //        carDamage = 5;
+        //        Debug.Log(carDamage);
+        //        StartCoroutine(DamageDelay(carDamage, 1f)); //코루틴으로 순간적으로 충돌이 중복적으로 일어나는 현상을 방지하기 위해 최소한의 딜레이값 설정필요함
+        //    }
+        //}
+        //else if (collision.gameObject.CompareTag("Person"))
+        //{
+        //    if (!delayedDamage)
+        //    {
+        //        //soundManager.SfxPlay(SoundManager.Sfx.crashBuiling);
+        //        carDamage = 1;
+        //        Debug.Log(carDamage);
+        //        StartCoroutine(DamageDelay(carDamage, 1f)); //코루틴으로 순간적으로 충돌이 중복적으로 일어나는 현상을 방지하기 위해 최소한의 딜레이값 설정필요함
+        //    }
+        //}
+    }
+    private IEnumerator DamageDelay(int carDamage, float delay)
+    {
+        delayedDamage = true;
+        CarDamage(carDamage); //Invoke 함수 썼을때는 박자마자 Log 출력 안되고 1초 딜레이가 처음부터 걸려버림 그래서 박자마자 실행후
+        yield return new WaitForSeconds(delay); // 딜레이를 걸어줌
+        delayedDamage = false;
+    }
 }
--- Assets/Script/Car/DamageCar.cs
+++ Assets/Script/Car/DamageCar.cs
@@ -4,46 +4,5 @@ using UnityEngine;
 
 public class DamageCar : AbstractCar
 {
-    public int carDamage;
-    private bool delayedDamage = false;
-
-    private void OnCollisionEnter(Collision collision) // 충돌이 일어나자 마자
-    {
-        if (collision.gameObject.CompareTag("building")) // building 이라는 tag 를 가진 오브젝트와 충돌했을때, CompareTag = 자바 Equals랑 비슷한 느낌?
-        {
-            if (!delayedDamage)
-            {
-                //soundManager.SfxPlay(SoundManager.Sfx.crashBuiling);
-                carDamage = 10;
-                Debug.Log(carDamage);
-                StartCoroutine(DamageDelay(carDamage, 1f)); //코루틴으로 순간적으로 충돌이 중복적으로 일어나는 현상을 방지하기 위해 최소한의 딜레이값 설정필요함
-            }
-        }
-        else if (collision.gameObject.CompareTag("CarDrag"))
-        {
-            if (!delayedDamage)
-            {
-                //soundManager.SfxPlay(SoundManager.Sfx.crashBuiling);
-                carDamage = 5;
-                Debug.Log(carDamage);
-                StartCoroutine(DamageDelay(carDamage, 1f)); //코루틴으로 순간적으로 충돌이 중복적으로 일어나는 현상을 방지하기 위해 최소한의 딜레이값 설정필요함
-            }
-        }else if (collision.gameObject.CompareTag("Person"))
-        {
-            if (!delayedDamage)
-            {
-                //soundManager.SfxPlay(SoundManager.Sfx.crashBuiling);
-                carDamage = 1;
-                Debug.Log(carDamage);
-                StartCoroutine(DamageDelay(carDamage, 1f)); //코루틴으로 순간적으로 충돌이 중복적으로 일어나는 현상을 방지하기 위해 최소한의 딜레이값 설정필요함
-            }
-        }
-    }
-    private IEnumerator DamageDelay(int carDamage, float delay)
-    {
-        delayedDamage = true;
-        CarDamage(carDamage); //Invoke 함수 썼을때는 박자마자 Log 출력 안되고 1초 딜레이가 처음부터 걸려버림 그래서 박자마자 실행후
-        yield return new WaitForSeconds(delay); // 딜레이를 걸어줌
-        delayedDamage = false;
-    }
+    
 }
