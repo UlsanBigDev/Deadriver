a7c9c8fe2f8a7c688d279189098a6fa7d9595981
ptk57581 <ptk725739@gmail.com>
2023-08-25 01:48:13 +0900

    Crash Mission 오브젝트 지정 가능

--- Assets/Script/Player/Mission/CrashMission.cs
+++ Assets/Script/Player/Mission/CrashMission.cs
@@ -2,20 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CrashType
+{
+    ALL, BUILDING, ENEMY_CAR, PERSON
+}
 public class CrashMission : AbstractMission, CrashListener
 {
     private int count;
+    private CrashType crashType;
     public CrashMission(int count)
     {
         this.count = count;
         isSuccess = true;
         Car.AddCrashListener(this);
+        crashType = CrashType.ALL;
     }
 
-    public void OnCrash(Enemy enemey)
+    public CrashMission(int count, CrashType type) : this(count) {
+        crashType = type;
+    }
+
+    public virtual void OnCrash(Enemy enemy)
     {
         if (!isSuccess) return;
+        if (!CheckType(enemy)) return;
         if (--count <= 0) isSuccess = false;
-        Debug.Log("Crash Mission : " + enemey);
+        Debug.Log("Crash Mission : " + enemy);
+    }
+
+    private bool CheckType(Enemy enemy)
+    {
+        if (crashType == CrashType.ALL)
+        {
+            return true;
+        }
+        else if (crashType == CrashType.BUILDING)
+        {
+            if (enemy is Building) return true;
+            else return false;
+        }
+        else if (crashType == CrashType.ENEMY_CAR)
+        {
+            if (enemy is EnemyCar) return true;
+            else return false;
+        }
+        else if (crashType == CrashType.PERSON)
+        {
+            if (enemy is Person) return true;
+            else return false;
+        }
+        else
+        { // 나도 모르는 경우
+            return false;
+        } 
     }
 }
