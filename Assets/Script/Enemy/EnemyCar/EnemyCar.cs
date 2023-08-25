8ca5b8411b3682f15285434ad72cf605a28724c6
ptk57581 <ptk725739@gmail.com>
2023-08-26 01:41:13 +0900

    enemy car 충돌 이벤트

--- Assets/Script/Enemy/EnemyCar/EnemyCar.cs
+++ Assets/Script/Enemy/EnemyCar/EnemyCar.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class EnemyCar : AbstractEnemy
 {
-    /* 자동차랑 충돌됐는지 알려주는 코드 */
-    //자동차랑 충돌되면 이 스크립트가 실행
-
+    public float speed;
+    private void Update()
+    {
+        transform.position += transform.forward.normalized * Time.deltaTime * speed;
+    }
+    
     protected override void OnPlayerCrash(Player player)
     {
-        //Debug.Log("자동차 충돌");
+
     }
 }
