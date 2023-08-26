e472a7e02535bed840427f152338032e6c0bdaeb
ptk57581 <ptk725739@gmail.com>
2023-08-26 14:15:50 +0900

    Pool Manager Get Function

--- Assets/Script/PoolManager.cs
+++ Assets/Script/PoolManager.cs
@@ -7,22 +7,40 @@ public class PoolManager : MonoBehaviour
     [SerializeField]
     private GameObject[] prefabs;
 
-    private List<GameObject>[] gameObjects;
+    private Dictionary<int, List<GameObject>> poolMap;
 
     private void Awake()
     {
-        
+        poolMap = new Dictionary<int, List<GameObject>>();
+        for (int i=0; i<prefabs.Length; i++)
+        {
+            poolMap.Add(i, new List<GameObject>());
+        }
+        Debug.Log("초기화 완료!");
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Get(int id)
     {
-        
+        GameObject obj = null;
+        List<GameObject> poolList = poolMap[id];
+        foreach (GameObject poolObject in poolList) {
+            if (!poolObject.activeSelf)
+            {
+                obj = poolObject;
+                obj.SetActive(true);
+                break;
+            }
+        }
+
+        if (obj == null)
+        {
+            obj = Instantiate(prefabs[id], transform);
+            poolList.Add(obj);
+            /// poolList 에 추가한 요소가 poolMap 에서 적용 안될수 있음...
+        }
+
+
+        return obj;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
