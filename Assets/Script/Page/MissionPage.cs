d1c7ecb547bccfa2b3497f91258e04c56c8fe783
ptk57581 <ptk725739@gmail.com>
2023-08-27 23:29:54 +0900

    Mission Scene 기능 구현

--- Assets/Script/Page/MissionPage.cs
+++ Assets/Script/Page/MissionPage.cs
@@ -1,57 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MissionPage : MonoBehaviour
 {
-    private Dictionary<int, Mission> missionMap;
+    private Dictionary<int, (Mission, string)> missionMap;
     private Mission mission;
+    private string missionScript;
+
+    [SerializeField]
+    private TMP_Text missionText;
 
     private void Awake()
     {
         InitializeMission();
         SetRandomMission();
-        Debug.Log("랜덤 미션!" + mission.title);
+    }
+
+    private void Start()
+    {
+        missionText.text = missionScript;
+        Player.missionList.Add(mission);
     }
 
     private void InitializeMission()
     {
-        missionMap = new Dictionary<int, Mission>();
+        missionMap = new Dictionary<int, (Mission, string)>();
         missionMap.Add(0,
-            new GroupMission(
-                new List<Mission>()
-                {
-                    new TimeMission(120f),
-                    new CrashMission(3)
-                }
-                , "어머니 급 가정 방"
+            (
+                new GroupMission(
+                    new List<Mission>()
+                    {
+                        new TimeMission(120f),
+                        new CrashMission(3)
+                    }
+                    , "어머니 급 가정 방"
+                ),
+                "소광씨 지금 바쁘신가요? 고객사 서버가 지금 오류가 생겨서 급하게 회사로 나와주셔야 할 것 같아요 안태원 대리님은 출장 중이셔서 소광씨께 부탁드릴게요 ㅠㅠ"
             )
         );
 
         missionMap.Add(1,
-            new GroupMission(
-                new List<Mission>()
-                {
-                    new TimeMission(240f),
-                    new CrashMission(6)
-                }
-                , "5분 내로 오면 5만원"
+            (
+                new GroupMission(
+                    new List<Mission>()
+                    {
+                        new TimeMission(120f),
+                        new CrashMission(3)
+                    }
+                    , "어머니 급 가정 방"
+                ),
+                "소광씨 지금 바쁘신가요? 고객사 서버가 지금 오류가 생겨서 급하게 회사로 나와주셔야 할 것 같아요 안태원 대리님은 출장 중이셔서 소광씨께 부탁드릴게요 ㅠㅠ"
             )
         );
         missionMap.Add(2,
-            new GroupMission(
-                new List<Mission>()
-                {
-                    new TimeMission(300f),
-                    new CrashMission(9)
-                }
-                , "상사 호출"
+            (
+                new GroupMission(
+                    new List<Mission>()
+                    {
+                        new TimeMission(120f),
+                        new CrashMission(3)
+                    }
+                    , "어머니 급 가정 방"
+                ),
+                "소광씨 지금 바쁘신가요? 고객사 서버가 지금 오류가 생겨서 급하게 회사로 나와주셔야 할 것 같아요 안태원 대리님은 출장 중이셔서 소광씨께 부탁드릴게요 ㅠㅠ"
             )
         );
     }
 
     private void SetRandomMission()
     {
-        mission = missionMap[Random.Range(0, 3)];
+        int randomValue = Random.Range(0, 3);
+        mission = missionMap[randomValue].Item1;
+        missionScript = missionMap[randomValue].Item2;
+    }
+    public void ChangeDriveScene()
+    {
+        LoadingManager.sceneName = "Drive";
+        SceneManager.LoadScene("LoadingScene");
     }
 }
